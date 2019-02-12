55256df40964f6309b16a2c8243fad20be4b189f
Laís Pessine do Carmo <lais.carmo@lnls.br>
2019-02-12 13:05:40 -0200

    Move scan-utils installation to /usr/local/SOL/
    scan-utils is being moved from /root/ to /usr/local/SOL so the contents of this library can be read/executed by users other than root.
    
    Create forder /etc/xdg/scan-utils/, a suggested folder for adding beamline configuration.
    
    scan-utils path is added to PYTHONPATH env var.
    
    Every scan-utils script is added to /usr/local/bin/ so they can be called from anywhere.
    
    Every scan-utils sample config is added to /etc/xdg/scan-utils/ so they can be seen from anywhere.

--- installscanutils.sh
+++ installscanutils.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+apt-get install python3-tk -y
+
 pip3 install pyyaml docopt pyxdg
-cd /root
+mkdir -p /usr/local/SOL/
+cd /usr/local/SOL/
 git clone https://gitlab.cnpem.br/SOL/Projetos/scan-utils
+
+# Create folder for beamline config files
+mkdir -p /etc/xdg/scan-utils/
+chmod 777 /etc/xdg/scan-utils/
+
+echo "export PYTHONPATH=\$PYTHONPATH:/usr/local/SOL/scan-utils/" >> /etc/bash.bashrc
+
+# Link executable scripts of scan-utils
+for i in /usr/local/SOL/scan-utils/*; do
+    if [[ -x "$i" ]]
+    then
+        echo "File '$i' is executable"
+        ln -s $i /usr/local/bin/`basename $i`
+    fi
+done
+
+#Link sample config files so those scripts can be called from any dir
+for i in /usr/local/SOL/scan-utils/*.yml; do
+    ln -s $i /etc/xdg/scan-utils/`basename $i`
+done
+
