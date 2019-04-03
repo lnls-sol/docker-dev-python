58047345b81c16418cffb61a5b536c7c94e77700
Douglas Henrique C. de Araújo <douglas.araujo@sol22-linux.abtlus.org.br>
2019-04-03 10:40:07 -0300

    Temporally fix build cache

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export USER_ID=`id -u ${USER}` && export GROUP_ID=`id -g ${USER}` && docker-compose up -d --build
+export USER_ID=`id -u ${USER}` && export GROUP_ID=`id -g ${USER}` && docker-compose build --no-cache && docker-run -d
 
