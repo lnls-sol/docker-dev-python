5b29be05ab2137ed7c436e3000c3a51bda8ae3cc
Douglas Henrique C. de Araújo <douglas.araujo@sol22-linux.abtlus.org.br>
2019-04-03 11:01:44 -0300

    Correct the script to use docker-compose up

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export USER_ID=`id -u ${USER}` && export GROUP_ID=`id -g ${USER}` && docker-compose build --no-cache && docker-run -d
+export USER_ID=`id -u ${USER}` && export GROUP_ID=`id -g ${USER}` && docker-compose build --no-cache && docker-compose up -d
 
