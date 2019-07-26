46713f5b4d4886bb42175e381167ed0ddfd779ad
Laís Pessine do Carmo <lais.carmo@lnls.br>
2019-07-26 14:08:09 -0300

    Install pyqtgraph on develop branch
    This branch is being used due to bug fixes.

--- installpydm.sh
+++ installpydm.sh
@@ -7,5 +7,8 @@ git checkout tags/v1.6.5 -b v1.6.5
 # Set 'only-if-needed' so pyqt is kept in right version
 pip3 install --upgrade-strategy=only-if-needed .[all]
 
+# Reinstall pyqtgraph, on develop branch for bug fixes 
+pip3 install git+https://github.com/pyqtgraph/pyqtgraph@develop
+
 mv /tmp/pydm /usr/local/pydm-1.6.5
 echo 'export PYQTDESIGNERPATH=/usr/local/pydm-1.6.5' >> /etc/bash.bashrc
